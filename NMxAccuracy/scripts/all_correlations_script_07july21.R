6dcd24847aeb33e5eca687cfdfa3d1862a149802
jameswyngaarden <james.wyngaarden@temple.edu>
2021-07-08 08:44:21 -0400

    edits to correlation matrices
    correlation values from scatterplots and these correlation matrices are not matching up, need to troubleshoot

---
+++ NMxAccuracy/scripts/all_correlations_script_07july21.R
@@ -0,0 +1,40 @@
+## Script for correlations between Social Doors stats, NM values,  substance use
+## Jimmy Wyngaarden, 07 July 2021
+
+###############################################################################
+###############################################################################
+
+setwd("~/Documents/GitHub/neuromelanin/NMxAccuracy")
+maindir <- getwd()
+datadir <- file.path("~/Documents/GitHub/neuromelanin/NMxAccuracy/data/")
+
+# load packages
+library("readxl")
+library("ggpubr")
+library("plot")
+library("Hmisc")
+
+# import data
+df <- read_excel("~/Documents/GitHub/neuromelanin/NMxAccuracy/data/all_correlations_07july21.xlsx")
+head(df)
+
+mcor <- round(cor(df),4)
+
+mcor
+write.csv(mcor, 'correlationmatrix.csv')
+
+# from displayr website:
+mydata = df
+mydata.cor <- cor(df)
+mydata.cor
+
+mydata.rcorr = rcorr(as.matrix(df))
+mydata.rcorr
+
+mydata.coeff = mydata.rcorr$r
+mydata.p = mydata.rcorr$P
+write.csv(mydata.coeff, 'correlationmatrix_coeff.csv')
+write.csv(mydata.p, 'correlationmatrix_p.csv')
+
+###############################################################################
+
