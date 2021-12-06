db63cdb4713bdcce42296fb1b9cb85d1008ee895
jameswyngaarden <james.wyngaarden@temple.edu>
2021-12-06 12:41:22 -0500

    added substance_abuse_voxels correlation

--- NMxTotalUsexAccuracy/scripts/NMpaper_plots_11-24-2021.R
+++ NMxTotalUsexAccuracy/scripts/NMpaper_plots_11-24-2021.R
@@ -15,18 +15,27 @@ df_maineffects <- read_excel("~/Documents/GitHub/neuromelanin/NMxTotalUsexAccura
 df_moderation <- read_excel("~/Documents/GitHub/neuromelanin/NMxTotalUsexAccuracy/data/NMxPositiveAccuracy_moderation.xlsx")
 
 
-# # # # # # Plot: NM-MRI signal and Substance Abuse (i.e., Total_Use) # # # # # 
+# # # # # # Plot: NM-MRI signal and Substance Abuse  # # # # # 
 
-plot(df_maineffects$NM_full, df_maineffects$Total_Use, pch=19,
+plot(df_maineffects$NM_full, df_maineffects$Substance_Abuse, pch=19,
      main = "Correlation between NM-MRI Signal and Substance Abuse",
      xlab = "NM-MRI Signal",
      ylab = "Substance Abuse",
      col = "blue")
-abline(lm(df_maineffects$Total_Use ~ df_maineffects$NM_full))
+abline(lm(df_maineffects$Substance_Abuse ~ df_maineffects$NM_full))
 
-cor.test(df_maineffects$NM_full, df_maineffects$Total_Use, method = "pearson")
+cor.test(df_maineffects$NM_full, df_maineffects$Substance_Abuse, method = "pearson")
 
+# # # # # # Plot: NM-MRI signal and Substance Abuse  # # # # # 
 
+plot(df_maineffects$NM_full, df_maineffects$Substance_Abuse_Voxels, pch=19,
+     main = "Correlation between NM-MRI Signal and Substance Abuse Voxels Signal",
+     xlab = "NM-MRI Signal",
+     ylab = "Substance Abuse Voxels Signal (neg 05)",
+     col = "gray")
+abline(lm(df_maineffects$Substance_Abuse_Voxels ~ df_maineffects$NM_full))
+
+cor.test(df_maineffects$NM_full, df_maineffects$Substance_Abuse_Voxels, method = "pearson")
 
 # # Plot: Moderating effect of NM on striatal activation and substance abuse # #
 
